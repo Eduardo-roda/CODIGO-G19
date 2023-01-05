a5564754a74ed72d601f064d26153d02135bc409
Eduardo Rodriguez <eduardo.roda20@gmail.com>
2023-01-04 20:14:42 -0500

    semana05 JS

---
+++ G19/semana05/dia2/arreglos.js
@@ -0,0 +1,29 @@
+//cuantos datos tienes una variable 
+//una  airbale solo tiene un dato 
+
+// iun areglo es un conjunto de datos 
+// la form DE DECLARAR un arreglo  es parecido a declarar una vairbale 
+// sin embargo debemos colorcar [] despues del ingual , ejemplo
+
+//array
+//tener diferentes tipos de dsatos?
+
+const alumnos =["guillermo","ana","jorge",true,33,[]]
+let frutas = ["fresa","naranja","tomate","pitahaya"]
+
+console.log("alumnos", alumnos)
+console.log("frutas", frutas)
+
+
+//habra de forma de acceder a nu elemento en especifico 
+//como accedemos a un eleemnto 
+
+console.log("alumnos",alumnos[10])
+
+// se podra alterar un dato en un array
+// vamos a reasignar el valor del indice 0
+
+alumnos[0] = "jhair";
+console.log("reasignado", alumnos)
+
+//poemos crear un nuevo elemento a un array
---
+++ G19/semana05/dia2/script.js
@@ -0,0 +1,4 @@
+let oficina = ["hojas", "pisapapeles", "computadora"];
+let animales = ["perro", "leon", "pantera", "conejo"];
+
+console.log(oficina[0], animales[3]);
